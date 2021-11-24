f3ce16d8326ef6061f720bb99e3a052b0d318043
Abdul <AMalik@lcs.on.ca>
2021-11-24 11:31:49 -0500

    Initial Commit

---
+++ NameGame/NameGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameGameApp.swift
+//  NameGame
+//
+//  Created by Abdul Malik on 2021-11-24.
+//
+
+import SwiftUI
+
+@main
+struct NameGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
